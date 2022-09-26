b96a5c9c736e2ef009ac80c5cec7fb1215108790
Einar Omang <einar.omang@cognite.com>
2022-09-26 12:18:45 +0200

    Lint

--- cognite/ingestion/__init__.py
+++ cognite/ingestion/__init__.py
@@ -1,2 +1,2 @@
 __version__ = "0.1.0"
-from .ingester import Ingester
+from .ingester import Ingester  # noqa: F401
--- example.py
+++ example.py
@@ -1,9 +1,11 @@
-from datetime import datetime, timedelta
 import os
+from datetime import datetime, timedelta
+
+from cognite.client import ClientConfig, CogniteClient
+from cognite.client.credentials import OAuthClientCredentials
+
 from cognite.ingestion import Ingester
 from cognite.ingestion.config import DataPointsDestination
-from cognite.client import CogniteClient, ClientConfig
-from cognite.client.credentials import OAuthClientCredentials
 
 client = CogniteClient(
     ClientConfig(
@@ -14,31 +16,23 @@ client = CogniteClient(
             token_url=os.environ["TOKEN_URL"],
             client_id=os.environ["CLIENT_ID"],
             client_secret=os.environ["CLIENT_SECRET"],
-            scopes=[f"{(os.environ['BASE_URL'])}/.default"]
-        )
+            scopes=[f"{(os.environ['BASE_URL'])}/.default"],
+        ),
     )
 )
 
 with Ingester(
-    'MyIngester',
+    "MyIngester",
     '. as $orig | .values[] as $arr | {"value": $arr.value, "timestamp": $arr.time, "externalId": $orig.tagName}',
     cdf_client=client,
-    config=DataPointsDestination(
-        max_queue_size=10_000,
-        max_upload_interval=1,
-        data_set=None,
-        external_id=None
-    )
+    config=DataPointsDestination(max_queue_size=10_000, max_upload_interval=1, data_set=None, external_id=None),
 ) as ingester:
     for i in range(100_000):
         data = {
-            "values": [{
-                "value": i*2,
-                "time": (datetime.now() + timedelta(seconds=i*2)).isoformat()
-            }, {
-                "value": i*2 + 1,
-                "time": (datetime.now() + timedelta(seconds=i*2 + 1)).isoformat()
-            }],
-            "tagName": 'my-test-ts'
+            "values": [
+                {"value": i * 2, "time": (datetime.now() + timedelta(seconds=i * 2)).isoformat()},
+                {"value": i * 2 + 1, "time": (datetime.now() + timedelta(seconds=i * 2 + 1)).isoformat()},
+            ],
+            "tagName": "my-test-ts",
         }
         ingester.ingest(data)
